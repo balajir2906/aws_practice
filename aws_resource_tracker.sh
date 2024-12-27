f69b3348fc8e39adad45419fa8d0654e9055efce
Ubuntu <ubuntu@ip-172-31-25-14.eu-north-1.compute.internal>
2024-12-27 06:28:08 +0000

    first commit Verion1

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###################################
+#Author: Balaji R
+#Script: AWS_RESOURCE_TRACKER
+#Date: 25/12/2025
+#Info: Used to track the usage of aws resources
+#Version: v1
+###################################
+#Services - EC2,LAMBDA,S3,IAM.
+#
+#
+set -x
+
+echo "print the ec2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' > resource_tracker.txt
+
+echo "print the lambda functions"
+aws lambda list-functions >> resource_tracker.txt
+
+echo "print the s3 buckets"
+aws s3 ls >> resource_tracker.txt
+
+echo "print the IAM users"
+aws iam list-users >> resource_tracker.txt
+
+
